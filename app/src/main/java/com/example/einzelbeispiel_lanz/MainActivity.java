b3255e45726957b8d43ea328ef711ca3e512a348
Florian Lanz <florianlanz73@gmail.com>
2021-03-05 14:30:25 +0100

    Implemented text fields of exercise 2.1. Entered Number entry field for the Matricle number. Implemented Send button. On send click there is a entry check. First it is checked if the entry contains only digits. Second is a length check. The number must have a length of 8 digits. I set the errors on the EditText filed

--- app/src/main/java/com/example/einzelbeispiel_lanz/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel_lanz/MainActivity.java
@@ -3,13 +3,43 @@ package com.example.einzelbeispiel_lanz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button send;
+    TextView enterMat;
+    EditText matNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        send = findViewById(R.id.btn);
+        enterMat = findViewById(R.id.enterMat);
+        matNumber = findViewById(R.id.entryMatNumber);
+
+
+        send.setOnClickListener(v -> {
+            String input = matNumber.getText().toString();
+            for (int i = 0; i < input.length(); i++) {
+                if (!Character.isDigit(input.charAt(i))) {
+                    matNumber.setError("Es dürfen nur Zahlen verwendet werden!");
+                    return;
+                }
+            }
+            if (input.length() != 8) {
+                matNumber.setError("Die Matrikelnummer muss 8 Zahlen beinhalten!");
+                return;
+            }
+
+
+
+
+        });
 
     }
+
+
 }
